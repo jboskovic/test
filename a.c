d8b6fef294b53c53ad27e13d26e7b186c8731713
anathema6 <jzivovic96@gmail.com>
2019-11-01 17:04:26 +0100

    another message

---
+++ a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void write_message() {
+    printf("This is some message.\n");
+
+int main() {
+    
+    write_message();
+    
+    
+    
+    return 0;
+}
